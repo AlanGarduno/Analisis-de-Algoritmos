45f629ba06c4eb351b41349563fa770dbae27733
ricardotg34 <36803940+ricardotg34@users.noreply.github.com>
2018-03-04 16:00:18 -0600

    Script agregafo

---
+++ Practica1/entrada.sh
@@ -0,0 +1,28 @@
+./main 100 nums.txt 1 >>tiempos.txt
+./main 100 nums.txt 2 >>tiempos.txt
+./main 100 nums.txt 3 >>tiempos.txt
+./main 100 nums.txt 4 >>tiempos.txt
+./main 100 nums.txt 5 >>tiempos.txt
+./main 100 nums.txt 6 >>tiempos.txt
+
+./main 1000 nums.txt 1 >>tiempos.txt
+./main 1000 nums.txt 2 >>tiempos.txt
+./main 1000 nums.txt 3 >>tiempos.txt
+./main 1000 nums.txt 4 >>tiempos.txt
+./main 1000 nums.txt 5 >>tiempos.txt
+./main 1000 nums.txt 6 >>tiempos.txt
+
+./main 5000 nums.txt 1 >>tiempos.txt
+./main 5000 nums.txt 2 >>tiempos.txt
+./main 5000 nums.txt 3 >>tiempos.txt
+./main 5000 nums.txt 4 >>tiempos.txt
+./main 5000 nums.txt 5 >>tiempos.txt
+./main 5000 nums.txt 6 >>tiempos.txt
+
+./main 10000 nums.txt 1 >>tiempos.txt
+./main 10000 nums.txt 2 >>tiempos.txt
+./main 10000 nums.txt 3 >>tiempos.txt
+./main 10000 nums.txt 4 >>tiempos.txt
+./main 10000 nums.txt 5 >>tiempos.txt
+./main 10000 nums.txt 6 >>tiempos.txt
+
