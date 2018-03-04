dee3411658821f3d87a32b6ba938a1885ba5cc88
ricardotg34 <36803940+ricardotg34@users.noreply.github.com>
2018-03-04 21:59:26 +0000

    Actualizacion de tiempos

--- Practica1/ABB/sbt.h
+++ Practica1/ABB/sbt.h
@@ -4,9 +4,10 @@ typedef struct Nodo{
   struct Nodo *izq,*der;
 }Nodo;
 
+
 typedef Nodo Arbol;
 
 Nodo *crearNodo(long int n);
 void destruitNodo(Nodo *nodo);
 void insertar(Nodo **arbol, long int valor);
-void inOrden(Nodo *arbol);
+void inOrden(Nodo *arbol, long A[]);
