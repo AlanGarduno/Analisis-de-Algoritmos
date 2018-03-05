cc4afabc74d174ea35ee1999392a45b810d0b4c7
ricardotg34 <36803940+ricardotg34@users.noreply.github.com>
2018-03-05 04:42:30 +0000

    Update script

--- Practica1/entrada.sh
+++ Practica1/entrada.sh
@@ -1,28 +1,17 @@
-./main 100 nums.txt 1 >>tiempos.txt
-./main 100 nums.txt 2 >>tiempos.txt
-./main 100 nums.txt 3 >>tiempos.txt
-./main 100 nums.txt 4 >>tiempos.txt
-./main 100 nums.txt 5 >>tiempos.txt
-./main 100 nums.txt 6 >>tiempos.txt
+echo "alg	n		real			user			sys				porc" >tiempos.txt
+for i in 6 5 4 3 2 1; do
+	./main 100 nums.txt $i >>tiempos.txt
+./main 100 nums.txt $i >>tiempos.txt
+./main 1000 nums.txt $i >>tiempos.txt
+./main 5000 nums.txt $i >>tiempos.txt
+./main 10000 nums.txt $i >>tiempos.txt
+./main 50000 nums.txt $i >>tiempos.txt
+./main 100000 nums.txt $i >>tiempos.txt
+./main 200000 nums.txt $i >>tiempos.txt
+./main 400000 nums.txt $i >>tiempos.txt
+./main 600000 nums.txt $i >>tiempos.txt
+./main 800000 nums.txt $i >>tiempos.txt
+./main 1000000 nums.txt $i >>tiempos.txt
+done
 
-./main 1000 nums.txt 1 >>tiempos.txt
-./main 1000 nums.txt 2 >>tiempos.txt
-./main 1000 nums.txt 3 >>tiempos.txt
-./main 1000 nums.txt 4 >>tiempos.txt
-./main 1000 nums.txt 5 >>tiempos.txt
-./main 1000 nums.txt 6 >>tiempos.txt
-
-./main 5000 nums.txt 1 >>tiempos.txt
-./main 5000 nums.txt 2 >>tiempos.txt
-./main 5000 nums.txt 3 >>tiempos.txt
-./main 5000 nums.txt 4 >>tiempos.txt
-./main 5000 nums.txt 5 >>tiempos.txt
-./main 5000 nums.txt 6 >>tiempos.txt
-
-./main 10000 nums.txt 1 >>tiempos.txt
-./main 10000 nums.txt 2 >>tiempos.txt
-./main 10000 nums.txt 3 >>tiempos.txt
-./main 10000 nums.txt 4 >>tiempos.txt
-./main 10000 nums.txt 5 >>tiempos.txt
-./main 10000 nums.txt 6 >>tiempos.txt
 
