ec3acc53b6854153377af2b7342c3ea6e8327300
AlanGarduno <agv_lrd@hotmail.com>
2018-05-30 23:06:27 -0500

    Landing page y menu completos

--- Proyecto/info.js
+++ Proyecto/info.js
@@ -1,13 +1,44 @@
 
+    $(document).ready(function(){
+        var estadoQ = false;
+        var estadoC = false;
+        var estadoD = false;
+        $('#btnQ').on('click', function(){
+            $('.sectionToggleQ').slideToggle();
+            if(estadoQ == true){
+                $(this).text("¿Qué es?");
+                estadoQ = false;
+            }
+            else{
+                $(this).text("Cerrar");
+                estadoQ = true;
+            }
+            
+        });
 
-function showInfo(){
-    alert("Hola");
-}
+        $('#btnC').on('click', function(){
+            $('.sectionToggleC').slideToggle();
+            if(estadoC == true){
+                $(this).text("¿Cómo funciona?");
+                estadoC = false;
+            }
+            else{
+                $(this).text("Cerrar");
+                estadoC = true;
+            }
+        });
 
+        $('#btnD').on('click', function(){
+            $('.sectionToggleD').slideToggle();
+            if(estadoD == true){
+                $(this).text("Demostración");
+                estadoD = false;
+            }
+            else{
+                $(this).text("Cerrar");
+                estadoD = true;
+            }
+        });    
+    });
 
-class DynamiContent{
-    static String;
-
-
-}
 
